f3df73626e65880cc2ae474162168e77c1e87c5c
Camilo Vanegas <Camilo.Vanegas@vml.com>
2018-12-27 17:45:48 -0500

    office fix

--- wp-content/themes/dep_construcciones/includes/results/php-logic.php
+++ wp-content/themes/dep_construcciones/includes/results/php-logic.php
@@ -1,14 +1,39 @@
 <?php
 	
 	// Post Variables
-
 	$type = $_POST['type'];
 	$business = $_POST['business'];
 	$area = $_POST['area'];
 	$from_home = $_POST['from-home'];
+	$args = [];
 
 	if($from_home == 1){
-		echo "Were from home";
+		$args = array (
+			'post_type' => 'inmueble',
+			'posts_per_page' => -1,
+			'tax_query' => array(
+				'relation' => 'AND',
+			    array(
+			        'taxonomy' => 'tipos',
+			        'terms' => $type,
+			        'field' => 'slug'
+			    ),
+			    array(
+			        'taxonomy' => 'transacciones',
+			        'terms' => $business,
+			        'field' => 'slug'
+			    )
+			),
+		);
+
 	} elseif ($from_home == 0) {
+
 		echo "Were from results";
+	} else {
+		$args = array(
+			'post_type' => 'inmueble',
+			'posts_per_page' => -1
+		);
 	}
+
+	$query = new WP_Query($args);
--- wp-content/themes/dep_construcciones/includes/results/side-form.php
+++ wp-content/themes/dep_construcciones/includes/results/side-form.php
@@ -2,10 +2,17 @@
 
 // Sideform block
 
+
+
 ?>
 
 <form action="<?php the_permalink(); ?>" method="post">
-	
+
+	<ul>
+		<li class="form-item form-type-select">
+			
+		</li>
+	</ul>
 	<input type="hidden" name="from-home" value="0">
 	<button type="submit">Buscar <i class="fa fa-search" aria-hidden="true"></i></button>
 
