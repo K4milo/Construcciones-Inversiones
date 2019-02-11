da52fda2ed198c749c5fc64dff327f3e32614b19
Camilo Vanegas <Camilo.Vanegas@vml.com>
2019-02-11 11:19:28 -0500

    php/ajustes variostpl

--- wp-content/themes/dep_construcciones/includes/loops/content-quotes.php
+++ wp-content/themes/dep_construcciones/includes/loops/content-quotes.php
@@ -13,7 +13,37 @@
         		<?php the_content()?>
         	</div>
     	</div>
+		
     </article>
 
 <?php endwhile; ?>
 <?php endif; ?>
+	<?php
+
+$icon_items = get_field('links_clientes');
+
+?>
+
+
+<div class="logos-wrapper">
+	<ul>
+	<?php 
+		if($icon_items):
+			while(have_rows('avaladadores')): the_row();
+
+				$imagen = get_sub_field('imagen-avalador');
+				
+			?>
+			<li class="icon_item">
+				<figure>
+					<img src="<?php echo $imagen ?>" width="170" height="auto">
+				</figure>
+				
+			</li>
+			<?php
+
+			endwhile;
+		endif;
+	?>
+	</ul>
+</div>
